b2a894c2d2f77c45a33109a65df35c14e91fdb6d
Ismael <ismael.dantas@hotmail.com>
2023-11-14 11:50:06 -0300

    Exercicio da aula Primeiro Servidor

---
+++ Meu-Primeiro-Servidor/src/index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('Ola mundo')
+})
+
+app.get('/array', (req, res) => {
+  const array = [
+    { id: 11, nome: 'joao', idade: 23 },
+    { id: 2, nome: 'maria', idade: 18 },
+    { id: 4, nome: 'ana', idade: 30 },
+    { id: 1, nome: 'rodrigo', idade: 17 },
+    { id: 123, nome: 'rodrigo', idade: 17 }
+  ]
+  res.send(array)
+})
+
+app.listen(3000)
