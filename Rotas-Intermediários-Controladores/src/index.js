154306e3470c70e5096cc3d8a39d260cb288e5ed
Ismael <ismael.dantas@hotmail.com>
2023-11-23 12:47:17 -0300

    Atualizando index.js com query e params

--- Rotas-Intermediários-Controladores/src/index.js
+++ Rotas-Intermediários-Controladores/src/index.js
@@ -2,8 +2,33 @@ const express = require('express')
 
 const app = express()
 
-app.get('/', (req, res) => {
-  res.send('Página inicial 1')
+const professores = [
+  { id: 1, nome: 'Ismael', stack: 'Backend' },
+  { id: 2, nome: 'Guido', stack: 'Backend' },
+  { id: 3, nome: 'Vidal', stack: 'Backend' },
+  { id: 4, nome: 'Dani', stack: 'Frontend' },
+  { id: 5, nome: 'Fernanda', stack: 'Frontend' }
+]
+
+app.get('/professores', (req, res) => {
+  const { stack } = req.query
+
+  let resultado = professores
+
+  if (stack) {
+    resultado = professores.filter(professor => {
+      return professor.stack === stack
+    })
+  }
+
+  res.send(resultado)
+})
+
+app.get('/professores/:id', (req, res) => {
+  const findProf = professores.find(professor => {
+    return professor.id === Number(req.params.id)
+  })
+  res.send(findProf)
 })
 
 app.listen(3000, () => {
